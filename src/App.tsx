c9fd8b9a89034bcac4d13b4f2c2ac557abb7927d
Todor Todorov <ttodorov@oxfordeconomics.com>
2020-01-27 15:36:48 +0000

    add React memo

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,42 @@ import React from 'react';
 import './App.css';
 
 interface ITableProps {
-  data: {Name: string, Id: number}[]
+  data: { Name: string, Id: number }[]
 }
 
-const Table: React.FC<ITableProps> = (props: ITableProps) => {
+const Table: React.FC<ITableProps> = React.memo((props: ITableProps) => {
   return (
     <div className="Table" key="Lambda" id="Lambda">
       <table>
         <tbody>
           <tr><th>Name</th><th>Id</th><th>Action</th></tr>
-          {props.data.map(element => 
+          {props.data.map(element =>
             <tr key={element.Id}><td>{element.Name}</td><td>{element.Id}</td><td><button onClick={() => alert("Click")}>Surprise</button></td></tr>
           )}
         </tbody>
       </table>
     </div>
   );
-}
+});
 
-const Table2: React.FC<ITableProps> = (props: ITableProps) => {
+const Table2: React.FC<ITableProps> = React.memo((props: ITableProps) => {
   return (
     <div className="Table" key="NoLambda" id="NoLambda">
       <table>
         <tbody>
           <tr><th>Name</th><th>Id</th><th>Action</th></tr>
-          {props.data.map(element => 
+          {props.data.map(element =>
             <tr key={element.Id}><td>{element.Name}</td><td>{element.Id}</td><td><button>No Surprise</button></td></tr>
           )}
         </tbody>
       </table>
     </div>
   );
-}
+});
 
 interface IInputProps {
   value: string;
-  setValue: (newValue:string) => void;
+  setValue: (newValue: string) => void;
 }
 
 const MyInput: React.FC<IInputProps> = (props: IInputProps) => {
@@ -55,22 +55,22 @@ class App extends React.Component<IAppProps, { Name: string }> {
 
   render() {
     return <div className="App">
-              <MyInput value={this.state.Name} setValue={v => this.setState(({Name: v}))} />
-              <input />
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      <Table data={this.props.data} />
-                    </td>
-                    <td>
-                      <Table2 data={this.props.data} />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              
-            </div>;
+      <MyInput value={this.state.Name} setValue={v => this.setState(({ Name: v }))} />
+      <input />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Table data={this.props.data} />
+            </td>
+            <td>
+              <Table2 data={this.props.data} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>;
   }
 }
 
