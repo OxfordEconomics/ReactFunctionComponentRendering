5d393a28a296d644f592787c806487bd2ab46b88
Steven Dawkins <sdawkins@oxfordeconomics.com>
2020-01-27 14:35:39 +0000

    simple example highlighting function components re-rendering when receiving unchanged props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+interface ITableProps {
+  data: {Name: string, Id: number}[]
+}
+
+const Table: React.FC<ITableProps> = (props: ITableProps) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Table" key="Lambda" id="Lambda">
+      <table>
+        <tbody>
+          <tr><th>Name</th><th>Id</th><th>Action</th></tr>
+          {props.data.map(element => 
+            <tr key={element.Id}><td>{element.Name}</td><td>{element.Id}</td><td><button onClick={() => alert("Click")}>Surprise</button></td></tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
+}
+
+const Table2: React.FC<ITableProps> = (props: ITableProps) => {
+  return (
+    <div className="Table" key="NoLambda" id="NoLambda">
+      <table>
+        <tbody>
+          <tr><th>Name</th><th>Id</th><th>Action</th></tr>
+          {props.data.map(element => 
+            <tr key={element.Id}><td>{element.Name}</td><td>{element.Id}</td><td><button>No Surprise</button></td></tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+interface IInputProps {
+  value: string;
+  setValue: (newValue:string) => void;
+}
+
+const MyInput: React.FC<IInputProps> = (props: IInputProps) => {
+  return (
+    <input value={props.value} onChange={e => props.setValue(e.target.value)} />
+  )
+}
+
+type IAppProps = ITableProps;
+
+class App extends React.Component<IAppProps, { Name: string }> {
+
+  state = { Name: "" }
+
+  render() {
+    return <div className="App">
+              <MyInput value={this.state.Name} setValue={v => this.setState(({Name: v}))} />
+              <input />
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <Table data={this.props.data} />
+                    </td>
+                    <td>
+                      <Table2 data={this.props.data} />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              
+            </div>;
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const data: {Name: string, Id: number}[] = [];
+
+for(var i = 0; i < 10000; i++)
+{
+  data.push({Name: "Item" + i, Id: i});
+}
+
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
